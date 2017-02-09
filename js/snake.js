ce467a810462af9d65fd4d0089b781487b321801
Jakub Siwiński <kubsiw@gmail.com>
2017-02-09 22:22:02 +0100

    html with canvas and some important vars in js created

--- js/snake.js
+++ js/snake.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
+    var myCanvas = document.getElementById('myCanvas');
+    var ctx = myCanvas.getContext('2d');
+    var snakeSize = 10;
+    var w = 350;
+    var h = 350;
+    var score = 0;
+    var snake;
+    var snakeSize = 10;
+    var food;
+    
+    
 }); // very last section of DOMContentLoaded
