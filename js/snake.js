5710fb574e9db1c663bd28a467afe911c24181bb
Jakub Siwiński <kubsiw@gmail.com>
2017-02-10 11:29:45 +0100

    add canvas elements

--- js/snake.js
+++ js/snake.js
@@ -11,4 +11,27 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var food;
     
     
+    var drawModule = function () {
+        
+        var bodySnake = function(x, y) {
+            ctx.fillStyle = 'green';
+            ctx.fillRect(x * snakeSize, y * snakeSize, snakeSize, snakeSize);
+        }
+        
+        var pizza = function(x, y) {
+            ctx.fillStyle = 'yellow';
+            ctx.fillRect(x * snakeSize, y * snakeSize, snakeSize, snakeSize);
+            ctx.fillStyle = 'red';
+            ctx.fillRect(x * snakeSize + 1, y * snakeSize + 1, snakeSize - 2,snakeSize - 2);
+        }
+        
+        var scoreText = function() {
+            var score_Text = 'Score: ' + score;
+            ctx.filltyle = 'blue';
+            ctx.fillText(score_text, 145, h-5);
+        }
+    }
+    
+    
+    
 }); // very last section of DOMContentLoaded
